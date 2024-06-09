faefdc3e5d78d088fb33018036a5062171e67a66
Lucas <lucasgomesmorais58@gmail.com>
2024-06-08 21:11:13 -0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import 'react-toastify/dist/ReactToastify.css'
+
+export default function Home() {
+  return (
+    <>
+      <main className="bg-background w-full h-screen flex items-center justify-center">
+        <div className="w-full max-w-[420px] bg-white rounded-xl drop-shadow-md p-8">
+        </div>
+      </main>
+    </>
+  )
+}
