cee844d5dd69bc284b297ff9668e2fca4eda692b
Lucas <lucasgomesmorais58@gmail.com>
2024-06-08 22:51:43 -0300

    feat: add home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 'use client'
+import { ImageUploader } from '@/components/ImageUploader';
+import { LoadingUpload } from '@/components/LoadingUpload';
+import { Toast } from '@/components/Toast';
+import { useState } from 'react'
 import 'react-toastify/dist/ReactToastify.css'
 
 export default function Home() {
+  const [isLoading, setisLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+
   return (
     <>
+      <Toast />
       <main className="bg-background w-full h-screen flex items-center justify-center">
         <div className="w-full max-w-[420px] bg-white rounded-xl drop-shadow-md p-8">
+          <ImageUploader />
         </div>
       </main>
     </>
