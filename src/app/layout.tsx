a1fb095af0f4d51f48894188614224fa1a6cb352
Lucas <lucasgomesmorais58@gmail.com>
2024-06-08 21:11:13 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ 
+  weight: '500',
+  subsets: ['latin'],
+  variable: '--font-poppins',
+});
+
+export const metadata: Metadata = {
+  title: "Upload your images",
+  description: "Upload your images and get the url",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} font-sans`}>{children}</body>
+    </html>
+  )
+}
