0455f2593fd002114ddff1568688320aca094ddc
Lucas <lucasgomesmorais58@gmail.com>
2024-06-22 23:42:05 -0300

    feat: add drag and drop features into ImageUploader

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
 import backgroundImage from "@/assets/image.svg";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 interface ImageUploaderProps {
     setIsLoading: (value: boolean) => void
@@ -7,12 +9,114 @@ interface ImageUploaderProps {
 }
 
 export function ImageUploader({ setIsLoading, setImageUrl }: ImageUploaderProps) {
+  const [isDragOver, setIsDragOver] = useState(false)
+
+  function imageTypeValidator(file: File) {
+    if (!file.type.startsWith("image/")) {
+      toast.error("Please select an image file")
+      return false
+    }
+
+    const acceptedImageTypes = [".jpeg", ".jpg", ".png"]
+
+    const fileExtension = file.name.split('.').pop()?.toLowerCase()
+
+    if (fileExtension) {
+      if (!acceptedImageTypes.includes(fileExtension)) {
+        toast.error("Unsupported file type. Please select an image file")
+        return false
+      }
+    }
+    return true
+  }
+
+  function handleDragOver(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault()
+    setIsDragOver(true)
+  }
+
+  async function handleDrop(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault()
+    const file = e.dataTransfer.files[0]
+    setIsDragOver(false)
+
+    if (!file) {
+      toast.error("No file selected. Please drag and drop an image file")
+      return
+    }
+    
+    try {
+      await handleSupabaseUpload(file)
+      toast.success('Image uploaded successfully')
+    } catch (error) {
+      console.error('Error uploading image:', error)
+      toast.error('Failed to upload image')
+    }
+  }
+
+  async function performUpload(file: File) {
+    setIsLoading(true)
+    
+    const formData = new FormData()
+    formData.append('file', file)
+
+    const response = await fetch('/api/upload', {
+      method: 'POST',
+      body: formData,
+    })
+
+    if (!response.ok) {
+      throw new Error('Upload failed')
+    }
+
+    const { imageUrl, error } = await response.json()
+
+    if (error) {
+      throw new Error(error)
+    }
+
+    return imageUrl
+  }
+
+  async function handleSupabaseUpload(file: File) {
+    try {
+      if (imageTypeValidator(file)) {
+        const imageUrl = await performUpload(file)
+        setImageUrl(imageUrl)
+        toast.success('Image uploaded successfully')
+      }
+    } catch (error) {
+      console.error(error)
+      toast.error('An error occurred while uploading the image')
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  async function handleFileInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0]
+
+    if (file) {
+      try {
+        await handleSupabaseUpload(file)
+        toast.success('File uploaded successfully')
+      } catch(error) {
+        console.error(error)
+        toast.error('An error occurred while uploading the file')
+      }
+    }
+  }
+
     return (
       <div className="flex flex-col items-center justify-start gap-5">
-        <h1 className="text-xl text-gray-500">Upload your image</h1>
-        <p className="text-xs text-gray-400">File should be Jpeg, Png,...</p>
+        <h1 className="text-xl text-gray-600 font-medium">Upload your image</h1>
+        <p className="text-xs text-gray-400 font-medium">File should be Jpeg, Png,...</p>
   
-        <div className="flex flex-col items-center justify-center w-full gap-10 p-10 border-2 border-dashed border-light-blue rounded-xl bg-gray-50">
+        <div
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+          className={`bg-gray-50 ${isDragOver ? 'border-green' : 'border-blue-200'} border-2 border-dashed rounded-xl p-10 flex items-center flex-col w-full justify-center gap-10`}
+        >
           <Image
             src={backgroundImage}
             alt="Default image"
@@ -20,16 +124,18 @@ export function ImageUploader({ setIsLoading, setImageUrl }: ImageUploaderProps)
             height={100}
             priority
           />
-          <h3 className="text-sm text-gray-300">Drag & Drop your image here</h3>
+          <h3 className="text-sm text-gray-300 font-medium">Drag & Drop your image here</h3>
         </div>
   
-        <p className="text-sm text-gray-200">Or</p>
+        <p className="text-sm text-gray-200 font-medium">Or</p>
   
         <div className="text-xs text-white bg-primary rounded-lg hover:brightness-90">
-          <label className="block py-3 px-5 cursor-pointer" htmlFor="fileInput">
+          <button 
+            onClick={handleFileInput}
+            className="px-4 py-2 bg-blue-500 text-white rounded-lg"
+          >
             Choose a file
-          </label>
-          <input type="file" className="hidden" id="fileInput" />
+          </button>
         </div>
       </div>
     );
